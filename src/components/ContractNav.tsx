c1c35d5472ceb94e98847801d8737957013e997e
YongjaeHyun <dltjrrbs2020@gmail.com>
2023-04-14 15:41:13 +0900

    [DEV 24] "내놓은 원룸" 페이지의 경로 변경에 따른 수정, 기존의 Nav 컴포넌트에서 ContractNav로 이름 변경

--- src/components/ContractNav.tsx
+++ src/components/ContractNav.tsx
@@ -2,23 +2,27 @@ import Link from "next/link";
 
 type Props = {
   completed: boolean;
+  isLandlord: boolean;
 };
 
-const Nav = ({ completed }: Props) => {
+const ContractNav = ({ completed, isLandlord }: Props) => {
   return (
     <>
       {completed ? (
         <div className="flex justify-evenly items-center rounded-xl w-full h-16 bg-gray-100 m-auto mb-10">
           <div>
             <Link
-              href="/board/landlord"
+              href={isLandlord ? "/landlord" : "/contract"}
               className="font-bold text-lg p-1"
             >
               진행중인 계약
             </Link>
           </div>
           <div>
-            <Link href="/board/landlord/completed" className="font-bold text-lg text-primary-500 border-b-2 p-1 border-primary-500">
+            <Link
+              href={isLandlord ? "/landlord/completed" : "contract/completed"}
+              className="font-bold text-lg text-primary-500 border-b-2 p-1 border-primary-500"
+            >
               완료된 계약
             </Link>
           </div>
@@ -27,14 +31,17 @@ const Nav = ({ completed }: Props) => {
         <div className="flex justify-evenly items-center rounded-xl w-full h-16 bg-gray-100 m-auto mb-10">
           <div>
             <Link
-              href="/board/landlord"
+              href={isLandlord ? "/landlord" : "/contract"}
               className="font-bold text-lg text-primary-500 border-b-2 p-1 border-primary-500"
             >
               진행중인 계약
             </Link>
           </div>
           <div>
-            <Link href="/board/landlord/completed" className="font-bold text-lg p-1">
+            <Link
+              href={isLandlord ? "/landlord/completed" : "contract/completed"}
+              className="font-bold text-lg p-1"
+            >
               완료된 계약
             </Link>
           </div>
@@ -44,4 +51,4 @@ const Nav = ({ completed }: Props) => {
   );
 };
 
-export default Nav;
+export default ContractNav;
